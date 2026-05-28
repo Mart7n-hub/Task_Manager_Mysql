7b9ff529eb04205ae24368f7b9e4af8d1adb4baa
Rychly_M <mrncz111@gmail.com>
2026-05-28 07:53:43 +0200

    Refactor database connection to use dotenv
    Add environment variable support for database connection

--- Task_Manager2/src/main.py
+++ Task_Manager2/src/main.py
@@ -1,13 +1,17 @@
 import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Funkce, která slouží pro kontrolu zapojení do databáze
 def pripojeni_db():
     try:
         conn = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="1111",
-            database="Task_manager"
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME")
         )
         print("Connected")
         return conn
